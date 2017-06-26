36d03bd18901206b68e1e30863d4652ab7cdd563
kuzmenko86 <kuzmenko86@gmail.com>
2017-06-26 23:40:07 +0300

    add whole project

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,9 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Admin on 6/26/2017.
  */
 public class HelloWorld {
+    public static void main(String[] args){
+
+        System.out.println("Hello world");
+    }
 }
